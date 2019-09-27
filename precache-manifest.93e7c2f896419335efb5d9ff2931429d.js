3458f681c4ddf46dcb3b71bc5b0646f8049985e6
fmiranda <filipe.m.mir@gmail.com>
2019-09-27 10:19:27 -0400

    Updates

---
+++ precache-manifest.93e7c2f896419335efb5d9ff2931429d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f99ed7f2199f2ebe6cd4b1f6fc30f2e3",
+    "url": "/police-incident-dashboard-react/index.html"
+  },
+  {
+    "revision": "d9003f7e33400437dafe",
+    "url": "/police-incident-dashboard-react/static/css/main.e2e00da6.chunk.css"
+  },
+  {
+    "revision": "78f84593b7a5e4767c1e",
+    "url": "/police-incident-dashboard-react/static/js/2.f8f8149a.chunk.js"
+  },
+  {
+    "revision": "d9003f7e33400437dafe",
+    "url": "/police-incident-dashboard-react/static/js/main.e62e3be2.chunk.js"
+  },
+  {
+    "revision": "f284de44bc748eecb325",
+    "url": "/police-incident-dashboard-react/static/js/runtime-main.81e25587.js"
+  }
+]);
