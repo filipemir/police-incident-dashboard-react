5c104cb99977a0f3cfba04dc34915e331aedd2c9
Filipe Miranda <filipe.m.mir@gmail.com>
2019-10-01 21:37:25 -0400

    Switch to react-leaflet

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
 import React from 'react';
-import Map from "./components/Map";
+import moment from 'moment';
+
+import IncidentMap from "./components/Map";
 import './styles/global.scss';
 
+const DATE_FORMAT = 'YYYY-MM-DD hh:mm';
+
 export default function App() {
-  return <Map />;
+  const endDate = moment().format(DATE_FORMAT),
+      startDate = moment().subtract(72, 'hours').format(DATE_FORMAT);
+
+  return <IncidentMap latLong={[42.3, -71.0589]} startDate={startDate} endDate={endDate} />;
 }
