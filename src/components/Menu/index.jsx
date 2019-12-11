691d2eb78613f2956ed54f590cdc928b73f11b15
Filipe Miranda <filipe.m.mir@gmail.com>
2019-12-10 21:09:20 -0500

    A bunch of styling

--- src/components/Menu/index.jsx
+++ src/components/Menu/index.jsx
@@ -5,8 +5,19 @@ import TimeframePicker from "../TimeframePicker";
 import IncidentGroupPicker from "../IncidentGroupPicker";
 
 export default function Menu({ timeframe, endDate, onTimeframeChange, onGroupToggled, incidentsByGroup, visibleGroups }) {
-  return <div className={"map-controls"}>
-    <TimeframePicker endDate={endDate} timeframe={timeframe} onTimeframeChange={onTimeframeChange} />
-    <IncidentGroupPicker incidentsByGroup={incidentsByGroup} visibleGroups={visibleGroups} onGroupToggled={onGroupToggled} />
+  return <div className={"menu-root"}>
+    <div className={"menu-title"}>
+      <h1>Boston <br /> Police Incidents</h1>
+    </div>
+    <div className={"menu-content"}>
+      <div className={"menu-section"}>
+        <h2 className={"menu-section-title"}>Date Range</h2>
+        <TimeframePicker endDate={endDate} timeframe={timeframe} onTimeframeChange={onTimeframeChange} />
+      </div>
+      <div className={"menu-section"}>
+        <h2 className={"menu-section-title"}>Incident Groups</h2>
+        <IncidentGroupPicker incidentsByGroup={incidentsByGroup} visibleGroups={visibleGroups} onGroupToggled={onGroupToggled} />
+      </div>
+    </div>
   </div>
 }
--- src/components/TimeframePicker/index.jsx
+++ src/components/TimeframePicker/index.jsx
@@ -1,19 +1,24 @@
 import React from "react";
+import Select from 'react-select';
 import isEqual from "lodash/isEqual";
 
 import "./styles.scss";
 import { TIMEFRAMES } from "../../constants/timeframes";
 
+const options = TIMEFRAMES.map(t => {
+  return { value: t, label: `${t.value} ${t.unit}` }
+});
+
 export default function TimeframePicker({ timeframe, endDate, onTimeframeChange }) {
   const timeframeIndex = TIMEFRAMES.findIndex(t => isEqual(t, timeframe));
 
   return <div className={"timeframe-picker"}>
-      <span>{`Timeframe (ending ${endDate.format("MM-DD-YY")}):`}</span>
-      <select className={"timeframe-picker__selector"} value={timeframeIndex} onChange={evt => onTimeframeChange(TIMEFRAMES[evt.target.value])}>
-        {TIMEFRAMES.map((t, i) => {
-          const { value, unit } = t;
-          return <option key={`timeframe-${i}`} value={i}>{`${value} ${unit}`}</option>
-        })}
-      </select>
+      <Select
+        className={"timeframe-picker__selector"}
+        value={options[timeframeIndex]}
+        onChange={t => onTimeframeChange(t.value)}
+        options={options}
+      />
+      <div className={"timeframe-picker__note"}>Sadly, for now this is actually ending on {`${endDate.format("MM-DD-YY")}`} because the data stopped flowing after that</div>
     </div>;
 }
