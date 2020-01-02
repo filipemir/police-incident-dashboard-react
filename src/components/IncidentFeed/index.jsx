31cb7891aab5320fae94965f425d5cfd2141bbe8
Filipe Miranda <filipe.m.mir@gmail.com>
2020-01-02 10:25:28 -0500

    Style feed

--- src/components/IncidentFeed/index.jsx
+++ src/components/IncidentFeed/index.jsx
@@ -7,13 +7,21 @@ import IncidentCard from '../IncidentCard';
  *
  * @param incidents {Incident[]}
  */
-export default function IncidentFeed({ incidents }) {
+export default function IncidentFeed({ incidents, onIncidentClick }) {
     return (
         <div className={'incident-feed'}>
             <h2 className={'incident-feed-title'}>Most Recent Incidents</h2>
-            {incidents.slice(0, 10).map(incident => {
-                return <IncidentCard key={`incident-card-${incident._clientSideId}`} incident={incident} />;
-            })}
+            <div className={'incident-feed-content'}>
+                {incidents.slice(0, 30).map(incident => {
+                    return (
+                        <IncidentCard
+                            key={`incident-card-${incident._clientSideId}`}
+                            incident={incident}
+                            onIncidentClick={onIncidentClick}
+                        />
+                    );
+                })}
+            </div>
         </div>
     );
 }
